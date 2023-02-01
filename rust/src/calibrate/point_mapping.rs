e050fb9b8607e299b8e63a0b397fb3a995366bab
Gavin J Stark <atthecodeface@gmail.com>
2023-02-01 23:16:53 +0000

    serde json working for point mapping set too

--- rust/src/calibrate/point_mapping.rs
+++ rust/src/calibrate/point_mapping.rs
@@ -86,26 +86,6 @@ impl NamedPointSet {
     }
 }
 
-//ft test_json
-#[test]
-fn test_json() -> Result<(), String> {
-    let mut nps = NamedPointSet::default();
-    nps.add_pt("fred", [1., 2., 3.].into());
-    let s = nps.to_json()?;
-    assert_eq!(s, r#"[["fred",[1.0,2.0,3.0]]]"#);
-    let nps = NamedPointSet::from_json(
-        r#"
-[["fred", [1, 2, 3]]]
-"#,
-    )?;
-    assert!(nps.get_pt("jim").is_none(), "Jim is not a point");
-    assert!(nps.get_pt("fred").is_some(), "Fred is a point");
-    assert_eq!(nps.get_pt("fred").unwrap().model()[0], 1.0);
-    assert_eq!(nps.get_pt("fred").unwrap().model()[1], 2.0);
-    assert_eq!(nps.get_pt("fred").unwrap().model()[2], 3.0);
-    Ok(())
-}
-
 //ip Serialize for NamedPointSet
 impl Serialize for NamedPointSet {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -137,10 +117,36 @@ impl<'de> Deserialize<'de> for NamedPointSet {
 }
 
 //a PointMappingSet
-//ip PointMappingSet
-#[derive(Default, Serialize, Deserialize)]
+//tp PointMappingSet
+#[derive(Default)]
 pub struct PointMappingSet {
     mappings: Vec<PointMapping>,
+}
+
+//ip Serialize for PointMappingSet
+impl Serialize for PointMappingSet {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeSeq;
+        let mut seq = serializer.serialize_seq(Some(self.mappings.len()))?;
+        for e in self.mappings.iter() {
+            seq.serialize_element(e)?;
+        }
+        seq.end()
+    }
+}
+
+//ip Deserialize for PointMappingSet
+impl<'de> Deserialize<'de> for PointMappingSet {
+    fn deserialize<DE>(deserializer: DE) -> Result<Self, DE::Error>
+    where
+        DE: serde::Deserializer<'de>,
+    {
+        let mappings = Vec::<PointMapping>::deserialize(deserializer)?;
+        Ok(Self { mappings })
+    }
 }
 
 //ip PointMappingSet
@@ -148,6 +154,38 @@ impl PointMappingSet {
     //fp new
     pub fn new() -> Self {
         Self::default()
+    }
+
+    //fp from_json
+    pub fn from_json(nps: &NamedPointSet, toml: &str) -> Result<Self, String> {
+        let mut pms: Self = serde_json::from_str(toml).map_err(|e| {
+            format!(
+                "Error in parsing json at line {} column {}: {}",
+                e.line(),
+                e.column(),
+                e
+            )
+        })?;
+        let errs = pms.rebuild_with_named_point_set(nps);
+        if errs.is_empty() {
+            Ok(pms)
+        } else {
+            let mut r = String::new();
+            let mut sep = "";
+            for e in errs {
+                r.push_str(&format!("{}'{}'", sep, e));
+                sep = ", ";
+            }
+            Err(format!(
+                "Failed to find points {} to map in named point set",
+                r
+            ))
+        }
+    }
+
+    //fp to_json
+    pub fn to_json(&self) -> Result<String, String> {
+        serde_json::to_string(self).map_err(|e| format!("{}", e))
     }
 
     //mp rebuild_with_named_point_set
@@ -185,7 +223,7 @@ impl PointMappingSet {
 
 //a PointMapping
 //tp PointMapping
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct PointMapping {
     /// The 3D model coordinate this point corresponds to
     ///
@@ -193,6 +231,32 @@ pub struct PointMapping {
     pub model: Rc<NamedPoint>,
     /// Screen coordinate
     pub screen: Point2D,
+}
+
+//ip Serialize for PointMapping
+impl Serialize for PointMapping {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeTuple;
+        let mut seq = serializer.serialize_tuple(2)?;
+        seq.serialize_element(self.model.name())?;
+        seq.serialize_element(&self.screen)?;
+        seq.end()
+    }
+}
+
+//ip Deserialize for PointMapping
+impl<'de> Deserialize<'de> for PointMapping {
+    fn deserialize<DE>(deserializer: DE) -> Result<Self, DE::Error>
+    where
+        DE: serde::Deserializer<'de>,
+    {
+        let (model_name, screen) = <(String, Point2D)>::deserialize(deserializer)?;
+        let model = Rc::new(NamedPoint::new(model_name, [0., 0., 0.].into()));
+        Ok(Self { model, screen })
+    }
 }
 
 //ip PointMapping
@@ -237,4 +301,46 @@ impl PointMapping {
     }
 
     //zz All done
+}
+//a Tests
+//ft test_json_0
+#[test]
+fn test_json_0() -> Result<(), String> {
+    let mut nps = NamedPointSet::default();
+    nps.add_pt("fred", [1., 2., 3.].into());
+    let s = nps.to_json()?;
+    assert_eq!(s, r#"[["fred",[1.0,2.0,3.0]]]"#);
+    let nps = NamedPointSet::from_json(
+        r#"
+[["fred", [1, 2, 3]]]
+"#,
+    )?;
+    assert!(nps.get_pt("jim").is_none(), "Jim is not a point");
+    assert!(nps.get_pt("fred").is_some(), "Fred is a point");
+    assert_eq!(nps.get_pt("fred").unwrap().model()[0], 1.0);
+    assert_eq!(nps.get_pt("fred").unwrap().model()[1], 2.0);
+    assert_eq!(nps.get_pt("fred").unwrap().model()[2], 3.0);
+    Ok(())
+}
+
+//ft test_json_1
+#[test]
+fn test_json_1() -> Result<(), String> {
+    let mut nps = NamedPointSet::default();
+    nps.add_pt("fred", [1., 2., 3.].into());
+    let mut pms = PointMappingSet::new();
+    pms.add_mapping(&nps, "fred", &[1., 2.].into());
+    let s = pms.to_json()?;
+    assert_eq!(s, r#"[["fred",[1.0,2.0]]]"#);
+    let pms = PointMappingSet::from_json(
+        &nps,
+        r#"
+[["fred", [1, 2]]]
+"#,
+    )?;
+    assert_eq!(pms.mappings().len(), 1);
+    assert_eq!(pms.mappings()[0].name(), "fred");
+    assert_eq!(pms.mappings()[0].screen()[0], 1.0);
+    assert_eq!(pms.mappings()[0].screen()[1], 2.0);
+    Ok(())
 }
