ad505032bd6a2717fbf5d76e87666f729b50e579
Gavin J Stark <atthecodeface@gmail.com>
2024-04-07 15:10:28 +0100

    More WGPU

--- image_calibrate/src/accel/accel_wgpu.rs
+++ image_calibrate/src/accel/accel_wgpu.rs
@@ -1,78 +1,92 @@
 use wgpu::util::DeviceExt;
 
-use std::{convert::TryInto, num::NonZeroU64};
-use wgpu::{BufferAsyncError, Device, Queue, RequestDeviceError, ShaderModule};
-
 use std::borrow::Cow;
-use std::future::Future;
-use std::pin::Pin;
 use std::str::FromStr;
-use std::task::Poll::Ready;
-pub struct Blah {}
 
-static VTABLE: &std::task::RawWakerVTable = &std::task::RawWakerVTable::new(
-    |x| std::task::RawWaker::new(x, VTABLE), // clone: unsafe fn(_: *const ()) -> RawWaker,
-    |_| (),                                  // wake: unsafe fn(_: *const ()),
-    |_| (),                                  // wake_by_ref: unsafe fn(_: *const ()),
-    |_| (),                                  // drop: unsafe fn(_: *const ())
-);
+use crate::utils::rtc::run_to_completion as rtc;
+pub struct AccelWgpu {
+    instance: wgpu::Instance,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    shaders: Vec<wgpu::ShaderModule>,
+}
 
-fn complete<T, F: Future<Output = T>>(mut thing: F) -> T {
-    let data = &() as *const ();
+impl AccelWgpu {
+    pub fn new() -> Self {
+        // Instantiates instance of WebGPU
+        let instance = wgpu::Instance::default();
 
-    let raw_waker = std::task::RawWaker::new(data, VTABLE);
-    let waker = unsafe { std::task::Waker::from_raw(raw_waker) };
-    let mut context = std::task::Context::from_waker(&waker);
-    let mut adapter = unsafe { Pin::new_unchecked(&mut thing) };
-    eprintln!("Waiting for thing");
-    loop {
-        eprintln!("Poll");
-        match adapter.as_mut().poll(&mut context) {
-            Ready(val) => {
-                eprintln!("Done waiting for thing");
-                return val;
-            }
-            _ => (),
+        // `request_adapter` instantiates the general connection to the GPU
+        let adapter =
+            rtc(instance.request_adapter(&wgpu::RequestAdapterOptions::default())).unwrap();
+
+        // `request_device` instantiates the feature specific connection to the GPU, defining some parameters,
+        //  `features` being the available features.
+        let (device, queue) = rtc(adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_defaults(),
+            },
+            None,
+        ))
+        .unwrap();
+        let shaders = vec![];
+        Self {
+            instance,
+            adapter,
+            device,
+            queue,
+            shaders,
         }
+    }
+    pub fn add_shader(&mut self, code: &str, opt_label: Option<&'static str>) -> usize {
+        let shader_desc = wgpu::ShaderModuleDescriptor {
+            label: opt_label,
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(code)),
+        };
+
+        // Loads the shader from WGSL
+        let sm = self.device.create_shader_module(shader_desc);
+        let n = self.shaders.len();
+        self.shaders.push(sm);
+        n
+    }
+    pub fn shader(&self, n: usize) -> Option<&wgpu::ShaderModule> {
+        self.shaders.get(n)
+    }
+    /*
+        pub fn create_buffer() -> {
+        let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size,
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+        }
+    */
+    pub fn device(&self) -> &wgpu::Device {
+        &self.device
+    }
+    pub fn queue(&self) -> &wgpu::Queue {
+        &self.queue
     }
 }
 
 fn execute_gpu(numbers: &[u32]) -> Option<Vec<u32>> {
-    // Instantiates instance of WebGPU
-    let instance = wgpu::Instance::default();
-
-    // `request_adapter` instantiates the general connection to the GPU
-    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions::default());
-    let adapter = complete(adapter).unwrap();
-    dbg!(&adapter);
-
-    // `request_device` instantiates the feature specific connection to the GPU, defining some parameters,
-    //  `features` being the available features.
-    let (device, queue) = complete(adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            label: None,
-            required_features: wgpu::Features::empty(),
-            required_limits: wgpu::Limits::downlevel_defaults(),
-        },
-        None,
-    ))
-    .unwrap();
-    dbg!(&device, &queue);
-
-    // Loads the shader from WGSL
-    let cs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
-        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
-    });
+    let mut accelerator = AccelWgpu::new();
 
     // Gets the size in bytes of the buffer.
     let size = std::mem::size_of_val(numbers) as wgpu::BufferAddress;
+
+    let cs_module = accelerator.add_shader(include_str!("shader.wgsl"), None);
 
     // Instantiates buffer without data.
     // `usage` of buffer specifies how it can be used:
     //   `BufferUsages::MAP_READ` allows it to be read (outside the shader).
     //   `BufferUsages::COPY_DST` allows it to be the destination of the copy.
-    let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+    let staging_buffer = accelerator.device().create_buffer(&wgpu::BufferDescriptor {
         label: None,
         size,
         usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
@@ -84,13 +98,16 @@ fn execute_gpu(numbers: &[u32]) -> Option<Vec<u32>> {
     //   A storage buffer (can be bound within a bind group and thus available to a shader).
     //   The destination of a copy.
     //   The source of a copy.
-    let storage_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("Storage Buffer"),
-        contents: bytemuck::cast_slice(numbers),
-        usage: wgpu::BufferUsages::STORAGE
-            | wgpu::BufferUsages::COPY_DST
-            | wgpu::BufferUsages::COPY_SRC,
-    });
+    let storage_buffer =
+        accelerator
+            .device()
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Storage Buffer"),
+                contents: bytemuck::cast_slice(numbers),
+                usage: wgpu::BufferUsages::STORAGE
+                    | wgpu::BufferUsages::COPY_DST
+                    | wgpu::BufferUsages::COPY_SRC,
+            });
 
     // A bind group defines how buffers are accessed by shaders.
     // It is to WebGPU what a descriptor set is to Vulkan.
@@ -102,26 +119,29 @@ fn execute_gpu(numbers: &[u32]) -> Option<Vec<u32>> {
     let pipeline_desc = wgpu::ComputePipelineDescriptor {
         label: None,
         layout: None,
-        module: &cs_module,
+        module: accelerator.shader(cs_module).unwrap(),
         entry_point: "main",
     };
-    let compute_pipeline = device.create_compute_pipeline(&pipeline_desc);
+    let compute_pipeline = accelerator.device().create_compute_pipeline(&pipeline_desc);
 
     // Instantiates the bind group, once again specifying the binding of buffers.
     let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
-    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        label: None,
-        layout: &bind_group_layout,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 0,
-            resource: storage_buffer.as_entire_binding(),
-        }],
-    });
+    let bind_group = accelerator
+        .device()
+        .create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: storage_buffer.as_entire_binding(),
+            }],
+        });
 
     // A command encoder executes one or many pipelines.
     // It is to WebGPU what a command buffer is to Vulkan.
-    let mut encoder =
-        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+    let mut encoder = accelerator
+        .device()
+        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
     {
         let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
             label: None,
@@ -137,7 +157,7 @@ fn execute_gpu(numbers: &[u32]) -> Option<Vec<u32>> {
     encoder.copy_buffer_to_buffer(&storage_buffer, 0, &staging_buffer, 0, size);
 
     // Submits command encoder for processing
-    queue.submit(Some(encoder.finish()));
+    accelerator.queue().submit(Some(encoder.finish()));
 
     // Note that we're not calling `.await` here.
     let buffer_slice = staging_buffer.slice(..);
@@ -148,7 +168,10 @@ fn execute_gpu(numbers: &[u32]) -> Option<Vec<u32>> {
     // Poll the device in a blocking manner so that our future resolves.
     // In an actual application, `device.poll(...)` should
     // be called in an event loop or on another thread.
-    device.poll(wgpu::Maintain::wait()).panic_on_timeout();
+    accelerator
+        .device()
+        .poll(wgpu::Maintain::wait())
+        .panic_on_timeout();
 
     // Awaits until `buffer_future` can be read from
     if let Ok(Ok(())) = receiver.recv() {
--- image_calibrate/src/utils.rs
+++ image_calibrate/src/utils.rs
@@ -4,6 +4,34 @@ use std::rc::Rc;
 use geo_nd::Vector;
 use serde::{Deserialize, Serialize};
 
+//a run_to_completion
+pub mod rtc {
+    use std::future::Future;
+    use std::task::Poll::Ready;
+    static RTC_VTABLE: &std::task::RawWakerVTable = &std::task::RawWakerVTable::new(
+        |x| std::task::RawWaker::new(x, RTC_VTABLE), // clone: unsafe fn(_: *const ()) -> RawWaker,
+        |_| (),                                      // wake: unsafe fn(_: *const ()),
+        |_| (),                                      // wake_by_ref: unsafe fn(_: *const ()),
+        |_| (),                                      // drop: unsafe fn(_: *const ())
+    );
+
+    //fp run_to_completion
+    pub fn run_to_completion<T, F: Future<Output = T>>(thing: F) -> T {
+        let data = &() as *const ();
+        let raw_waker = std::task::RawWaker::new(data, RTC_VTABLE);
+        let waker = unsafe { std::task::Waker::from_raw(raw_waker) };
+        let mut context = std::task::Context::from_waker(&waker);
+
+        let mut adapter = Box::pin(thing);
+        loop {
+            if let Ready(val) = adapter.as_mut().poll(&mut context) {
+                return val;
+            }
+        }
+    }
+}
+
+//a Rrc
 //tp Rrc
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Rrc<T>(Rc<RefCell<T>>);
