3eb6cde3ca5c14a8bcade44d4da2c0a819170359
Gavin Stark <atthecodeface@gmail.com>
2026-06-29 13:49:22 +0100

    ic_camera: Addeed 'pretty' option for camera database Json

--- photogram/ic_camera/src/camera_database.rs
+++ photogram/ic_camera/src/camera_database.rs
@@ -38,8 +38,12 @@ impl std::fmt::Display for CameraDatabase {
 }
 
 impl CameraDatabase {
-    pub fn to_json(&self) -> Result<String> {
-        Ok(serde_json::to_string(self)?)
+    pub fn to_json(&self, pretty: bool) -> Result<String> {
+        if pretty {
+            Ok(serde_json::to_string_pretty(self)?)
+        } else {
+            Ok(serde_json::to_string(self)?)
+        }
     }
 
     pub fn derive(&mut self) {
