a18363b0f15bafa378adfeda5faf8fa2e0e53eff
Gavin Stark <atthecodeface@gmail.com>
2026-09-24 14:51:07 +0100

    image_server: Updated to use Point2D rather than reference in pms mappings

--- photogram/image_server/src/project_set.rs
+++ photogram/image_server/src/project_set.rs
@@ -171,7 +171,7 @@ impl ProjectSet {
         let cip_r = cip.borrow();
 
         let pms = cip_r.pms();
-        let mesh = Mesh::optimized(pms.borrow().mappings().iter().map(|p| *p.screen()), 1E-2);
+        let mesh = Mesh::optimized(pms.borrow().mappings().iter().map(|p| p.screen()), 1E-2);
         let triangles: Vec<_> = mesh.triangles().collect();
         eprintln!("Triangles of mesh {triangles:?}");
         let json = serde_json::to_string(&triangles).unwrap();
